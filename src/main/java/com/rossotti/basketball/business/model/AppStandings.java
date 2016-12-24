cff8e945b6309c683c380e9b9a5909dabe743449
Paul Rossotti <rossotti.paul@gmail.com>
2016-12-24 08:19:19 -0800

    create business.model package

---
+++ src/main/java/com/rossotti/basketball/business/model/AppStandings.java
@@ -0,0 +1,28 @@
+package com.rossotti.basketball.business.model;
+
+import com.rossotti.basketball.jpa.model.Standing;
+
+import java.util.List;
+
+public class AppStandings {
+	private AppStatus appStatus;
+	public void setAppStatus(AppStatus appStatus) {
+		this.appStatus = appStatus;
+	}
+	public Boolean isAppCompleted() {
+		return appStatus == AppStatus.Completed;
+	}
+	public Boolean isAppClientError() {
+		return appStatus == AppStatus.ClientError;
+	}
+	public Boolean isAppServerError() {
+		return appStatus == AppStatus.ServerError;
+	}
+	private List<Standing> standings;
+	public List<Standing> getStandings() {
+		return standings;
+	}
+	public void setStandings(List<Standing> standings) {
+		this.standings = standings;
+	}
+}
---
+++ src/main/java/com/rossotti/basketball/business/model/AppStatus.java
@@ -0,0 +1,12 @@
+package com.rossotti.basketball.business.model;
+
+public enum AppStatus {
+	Completed,
+	ClientError,
+	ServerError,
+	RosterUpdate,
+	RosterComplete,
+	RosterError,
+	OfficialError,
+	TeamError
+}
