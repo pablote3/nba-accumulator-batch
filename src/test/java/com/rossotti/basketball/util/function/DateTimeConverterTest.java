7b86b71a73c2c0cfe974c5001fda29fd3c981da0
Paul Rossotti <rossotti.paul@gmail.com>
2016-12-27 05:55:12 -0800

    cleanup unused import

--- src/test/java/com/rossotti/basketball/util/function/DateTimeConverterTest.java
+++ src/test/java/com/rossotti/basketball/util/function/DateTimeConverterTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 
 public class DateTimeConverterTest {
