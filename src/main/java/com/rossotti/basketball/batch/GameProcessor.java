9647b26297630f3c43514e188939447310501dd6
Paul Rossotti <rossotti.paul@gmail.com>
2017-01-13 17:55:03 -0800

    introduce businessServices

--- src/main/java/com/rossotti/basketball/batch/GameProcessor.java
+++ src/main/java/com/rossotti/basketball/batch/GameProcessor.java
@@ -1,7 +1,8 @@
 package com.rossotti.basketball.batch;
 
-import com.rossotti.basketball.app.service.GameAppService;
-import com.rossotti.basketball.batch.model.GameReaderInput;
+import com.rossotti.basketball.business.model.GameBusiness;
+import com.rossotti.basketball.business.service.GameBusService;
+import com.rossotti.basketball.business.service.RosterPlayerBusService;
 import com.rossotti.basketball.jpa.model.Game;
 import com.rossotti.basketball.util.function.DateTimeConverter;
 import org.slf4j.Logger;
@@ -9,25 +10,23 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
-
 public class GameProcessor implements ItemProcessor<Game, Game> {
 
 	@Autowired
-	private GameAppService gameAppService;
+	private GameBusService gameBusService;
+
+	@Autowired
+	private RosterPlayerBusService rosterPlayerBusService;
 
 	private final Logger logger = LoggerFactory.getLogger(GameProcessor.class);
 
 	@Override
 	public Game process(Game game) throws Exception {
-//		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.S");
-//		LocalDate gameDate = LocalDate.parse(game.getGameDateTime(), formatter);
-//		logger.info("Game finder " + gameDate);
-//		Game game = gameAppService.findByTeamKeyAsOfDate(game.getTeamKey(), gameDate);
 		logger.info("Updating Game Status " + DateTimeConverter.getStringDate(game.getGameDateTime()));
-		logger.info("Game Away Team = " + game.getBoxScoreAway().getTeam().getAbbr());
-		game.setStatus(Game.GameStatus.Completed);
+		GameBusiness gameBusiness = gameBusService.scoreGame(game);
+		if (gameBusiness.isRosterUpdate()) {
+//			rosterPlayerBusService.loadRoster()
+		}
 		return game;
 	}
 }
