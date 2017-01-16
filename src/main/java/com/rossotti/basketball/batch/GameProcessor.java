c940c8df65eb3550eb35e4da98b197c16ffe185d
Paul Rossotti <rossotti.paul@gmail.com>
2017-01-15 22:32:43 -0800

    call business services and introduce exception handling

--- src/main/java/com/rossotti/basketball/batch/GameProcessor.java
+++ src/main/java/com/rossotti/basketball/batch/GameProcessor.java
@@ -1,6 +1,9 @@
 package com.rossotti.basketball.batch;
 
+import com.rossotti.basketball.batch.exception.RosterRebuildException;
+import com.rossotti.basketball.batch.exception.SkipStepException;
 import com.rossotti.basketball.business.model.GameBusiness;
+import com.rossotti.basketball.business.model.RosterPlayerBusiness;
 import com.rossotti.basketball.business.service.GameBusService;
 import com.rossotti.basketball.business.service.RosterPlayerBusService;
 import com.rossotti.basketball.jpa.model.Game;
@@ -22,11 +25,24 @@ public class GameProcessor implements ItemProcessor<Game, Game> {
 
 	@Override
 	public Game process(Game game) throws Exception {
-		logger.info("Updating Game Status " + DateTimeConverter.getStringDate(game.getGameDateTime()));
+		String gameDate = DateTimeConverter.getStringDate(game.getGameDateTime());
+		logger.info("GameProcessor gameDate: " + gameDate + " awayTeam: " + game.getBoxScoreAway().getTeam().getAbbr() + " homeTeam: " + game.getBoxScoreHome().getTeam().getAbbr());
 		GameBusiness gameBusiness = gameBusService.scoreGame(game);
 		if (gameBusiness.isRosterUpdate()) {
-//			rosterPlayerBusService.loadRoster()
+			RosterPlayerBusiness rosterPlayerBusiness = rosterPlayerBusService.loadRoster(gameDate, gameBusiness.getRosterLastTeam());
+			if (rosterPlayerBusiness.isCompleted()) {
+				throw new RosterRebuildException("RosterLoad complete - gameDate: " + gameDate + " team: " + gameBusiness.getRosterLastTeam());
+			}
+			else {
+				throw new SkipStepException("RosterLoad problem - status code: " + rosterPlayerBusiness.getStatusCode());
+			}
 		}
-		return game;
+		else if (gameBusiness.isCompleted()) {
+			logger.info("GameScore complete " + DateTimeConverter.getStringDate(game.getGameDateTime()));
+			return game;
+		}
+		else {
+			throw new SkipStepException("GameScore problem - status code: " + gameBusiness.getStatusCode());
+		}
 	}
 }
