7ec6fcfa013fb9dd0d88ac6e7c7cd3eda988ca63
Paul Rossotti <rossotti.paul@gmail.com>
2017-01-15 22:32:14 -0800

    initial commit

---
+++ src/main/java/com/rossotti/basketball/batch/exception/RosterRebuildException.java
@@ -0,0 +1,7 @@
+package com.rossotti.basketball.batch.exception;
+
+public class RosterRebuildException extends Exception {
+	public RosterRebuildException(String message) {
+		super(message);
+	}
+}
---
+++ src/main/java/com/rossotti/basketball/batch/exception/SkipStepException.java
@@ -0,0 +1,7 @@
+package com.rossotti.basketball.batch.exception;
+
+public class SkipStepException extends Exception {
+	public SkipStepException(String message) {
+		super(message);
+	}
+}
