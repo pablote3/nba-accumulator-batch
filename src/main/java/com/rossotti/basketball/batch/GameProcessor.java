1a0785e720e159ff0b390790e487aa6180b74996
Paul Rossotti <rossotti.paul@gmail.com>
2017-02-01 19:05:06 -0800

    added scoreGame()

--- src/main/java/com/rossotti/basketball/batch/GameProcessor.java
+++ src/main/java/com/rossotti/basketball/batch/GameProcessor.java
@@ -1,9 +1,9 @@
 package com.rossotti.basketball.batch;
 
-import com.rossotti.basketball.batch.exception.RosterRebuildException;
 import com.rossotti.basketball.batch.exception.SkipStepException;
 import com.rossotti.basketball.business.model.GameBusiness;
 import com.rossotti.basketball.business.model.RosterPlayerBusiness;
+import com.rossotti.basketball.business.model.StatusCodeBusiness;
 import com.rossotti.basketball.business.service.GameBusService;
 import com.rossotti.basketball.business.service.RosterPlayerBusService;
 import com.rossotti.basketball.jpa.model.Game;
@@ -22,27 +22,49 @@ public class GameProcessor implements ItemProcessor<Game, Game> {
 	private RosterPlayerBusService rosterPlayerBusService;
 
 	private final Logger logger = LoggerFactory.getLogger(GameProcessor.class);
+	private String rosterLastTeam;
 
 	@Override
 	public Game process(Game game) throws Exception {
 		String gameDate = DateTimeConverter.getStringDate(game.getGameDateTime());
 		logger.info("GameProcessor gameDate: " + gameDate + " awayTeam: " + game.getBoxScoreAway().getTeam().getAbbr() + " homeTeam: " + game.getBoxScoreHome().getTeam().getAbbr());
-		GameBusiness gameBusiness = gameBusService.scoreGame(game);
-		if (gameBusiness.isRosterUpdate()) {
-			RosterPlayerBusiness rosterPlayerBusiness = rosterPlayerBusService.loadRoster(gameDate, gameBusiness.getRosterLastTeam());
-			if (rosterPlayerBusiness.isCompleted()) {
-				throw new RosterRebuildException("RosterLoad complete - gameDate: " + gameDate + " team: " + gameBusiness.getRosterLastTeam());
-			}
-			else {
-				throw new SkipStepException("RosterLoad problem - status code: " + rosterPlayerBusiness.getStatusCode());
+		GameBusiness gameBusiness = new GameBusiness(game);
+		gameBusiness = this.scoreGame(gameBusiness);
+		if (gameBusiness.isRosterComplete()) {
+			gameBusiness = this.scoreGame(gameBusiness);
+			if (gameBusiness.isRosterComplete()) {
+				gameBusiness = this.scoreGame(gameBusiness);
 			}
 		}
-		else if (gameBusiness.isCompleted()) {
+		if (gameBusiness.isCompleted()) {
 			logger.info("GameScore complete " + DateTimeConverter.getStringDate(game.getGameDateTime()));
 			return game;
 		}
 		else {
 			throw new SkipStepException("GameScore problem - status code: " + gameBusiness.getStatusCode());
 		}
+	}
+
+	private GameBusiness scoreGame(GameBusiness gameBusiness) throws Exception {
+		gameBusiness = gameBusService.scoreGame(gameBusiness.getGame());
+		if (gameBusiness.isClientError() || gameBusiness.isServerError() || gameBusiness.isOfficialError() || gameBusiness.isTeamError()) {
+			throw new SkipStepException("ScoreGame problem - status code: " + gameBusiness.getStatusCode());
+		}
+		else if (gameBusiness.isRosterUpdate()) {
+			if (!rosterLastTeam.equals(gameBusiness.getRosterLastTeam())) {
+				rosterLastTeam = gameBusiness.getRosterLastTeam();
+				RosterPlayerBusiness rosterPlayerBusiness = rosterPlayerBusService.loadRoster(DateTimeConverter.getStringDate(gameBusiness.getGame().getGameDateTime()), gameBusiness.getRosterLastTeam());
+				if (rosterPlayerBusiness.isClientError() || rosterPlayerBusiness.isServerError()) {
+					throw new SkipStepException("RosterLoad problem - status code: " + rosterPlayerBusiness.getStatusCode());
+				}
+				else if (rosterPlayerBusiness.isCompleted()) {
+					gameBusiness.setStatusCode(StatusCodeBusiness.StatusCode.RosterComplete);
+				}
+			}
+			else {
+				throw new SkipStepException("Roster already updated for team : " + rosterLastTeam);
+			}
+		}
+		return gameBusiness;
 	}
 }
