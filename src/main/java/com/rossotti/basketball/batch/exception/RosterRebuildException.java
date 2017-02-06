e8cf6c6b9a468902ec49255be377a5ed1f9d1957
Paul Rossotti <rossotti.paul@gmail.com>
2017-02-05 20:23:35 -0800

    not necessary exception

--- src/main/java/com/rossotti/basketball/batch/exception/RosterRebuildException.java
+++
@@ -1,7 +0,0 @@
-package com.rossotti.basketball.batch.exception;
-
-public class RosterRebuildException extends Exception {
-	public RosterRebuildException(String message) {
-		super(message);
-	}
-}
