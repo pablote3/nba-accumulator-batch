805121f7b905d73b85e51f1da94608815df98998
Paul Rossotti <rossotti.paul@gmail.com>
2017-02-07 07:04:49 -0800

    initial commit

---
+++ src/main/java/com/rossotti/basketball/batch/StandingsTasklet.java
@@ -0,0 +1,38 @@
+package com.rossotti.basketball.batch;
+
+import com.rossotti.basketball.batch.exception.SkipStepException;
+import com.rossotti.basketball.business.model.StandingsBusiness;
+import com.rossotti.basketball.business.service.StandingBusService;
+import com.rossotti.basketball.util.function.DateTimeConverter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDate;
+
+public class StandingsTasklet implements Tasklet {
+
+	@Autowired
+	private StandingBusService standingBusService;
+
+	private final Logger logger = LoggerFactory.getLogger(StandingsTasklet.class);
+
+	@Override
+	public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+		LocalDate gameDate = LocalDate.of(2016, 11, 5);
+		String asOfDate = DateTimeConverter.getStringDate(gameDate);
+		StandingsBusiness standingsBusiness = standingBusService.rankStandings(asOfDate);
+
+		if (standingsBusiness.isCompleted()) {
+			logger.info("RankStandings complete " + asOfDate);
+			return RepeatStatus.FINISHED;
+		}
+		else {
+			logger.info("RankStandings problem - status code: " + standingsBusiness.getStatusCode());
+			throw new SkipStepException("RankStandings problem - status code: " + standingsBusiness.getStatusCode());
+		}
+	}
+}
