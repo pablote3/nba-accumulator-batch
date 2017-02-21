edb9f6a4e22eee8044c0a9c571946e0021f89971
Paul Rossotti <rossotti.paul@gmail.com>
2017-02-20 21:27:19 -0800

    additional messageChannels, queueChannel, and poller

--- src/main/java/com/rossotti/basketball/config/IntegrationConfig.java
+++ src/main/java/com/rossotti/basketball/config/IntegrationConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.integration.annotation.IntegrationComponentScan;
 import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.channel.QueueChannel;
 import org.springframework.integration.config.EnableIntegration;
+import org.springframework.integration.scheduling.PollerMetadata;
 import org.springframework.messaging.MessageChannel;
 
 @Configuration
@@ -25,7 +27,40 @@ public class IntegrationConfig {
     }
 
     @Bean
+    public MessageChannel gameSplitterChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public QueueChannel gameRouterChannel() {
+        return new QueueChannel(20);
+    }
+
+    @Bean
+    public MessageChannel gameScoreChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel gameResultsChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    public MessageChannel gameAggregatorChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
     public MessageChannel outputChannel() {
         return new DirectChannel();
+    }
+
+    @Bean(name = PollerMetadata.DEFAULT_POLLER)
+    public PollerMetadata defaultPoller() {
+        PollerMetadata pollerMetadata = new PollerMetadata();
+        pollerMetadata.setMaxMessagesPerPoll(1L);
+ //       pollerMetadata.setTrigger(new PeriodicTrigger(10));
+        return pollerMetadata;
     }
 }
