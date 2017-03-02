a334e10c4f51bce97d852c5521ae3e6cb8c6a819
Paul Rossotti <rossotti.paul@gmail.com>
2017-03-01 17:08:12 -0800

    rename application tests

--- src/test/java/com/rossotti/basketball/ScoreGameTests.java
+++ src/test/java/com/rossotti/basketball/ScoreGameTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class NbaAccumulatorBatchApplicationTests {
+public class ScoreGameTests {
 
 	@Test
 	public void contextLoads() {
